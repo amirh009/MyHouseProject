8a7818af2c53425f1a8287dbed99aabe039040ed
amirh009 <amir.heidari.1998@gmail.com>
2014-10-17 16:42:06 -0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+// JavaScript Document
+document.write('<h1><center><strong><a href="index_mine.html">House Project</a></strong></center></h1>');
+document.write('<p><center>');
+document.write('<a href="Static_House_mine.html">Static House</a>'); 
+document.write('  ');
+document.write('<a href="comparison_mine.html">Comparison</a>');
+document.write('  ');
+document.write('<a href="Dynamic_House_mine.html">Dynamic House</a>');
+document.write('</center></p>'); 
